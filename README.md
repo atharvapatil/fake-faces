6c4be849a442547bfe73dd2b36c2c2b94d045e8a
atharvapatil <atharvaabhaypatil@gmail.com>
2019-09-24 18:09:49 -0400

    Working Component

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# fake-faces
+# Fake Profile Picture Generator.
+
---
+++ fake-faces.framerfx/code/FakeProfile.tsx
@@ -0,0 +1,74 @@
+import * as React from "react"
+import { Frame, addPropertyControls, ControlType } from "framer"
+
+export function FakeProfile(props) {
+    const { size, bound, boundColor, boundDisplay, radius } = props
+
+    return (
+        <Frame
+            center
+            size={bound}
+            backgroundColor={boundDisplay ? boundColor : "transparent"}
+        >
+            <Frame
+                center
+                size={size}
+                image="https://thispersondoesnotexist.com/image"
+                borderRadius={radius + "%"}
+                overflow={"hidden"}
+            />
+        </Frame>
+    )
+}
+
+FakeProfile.defaultProps = {
+    size: 148,
+    bound: 180,
+    boundColor: "#f7e8f6",
+    boundDisplay: true,
+    radius: 50,
+}
+
+addPropertyControls(FakeProfile, {
+    size: {
+        min: 24,
+        max: 240,
+        title: "Image Size",
+        type: ControlType.Number,
+        defaultValue: 148,
+        step: 4,
+        displayStepper: true,
+    },
+
+    bound: {
+        min: 24,
+        max: 280,
+        title: "Bound Size",
+        type: ControlType.Number,
+        defaultValue: 180,
+        step: 4,
+        displayStepper: true,
+    },
+
+    boundColor: {
+        title: "Bound Color",
+        type: ControlType.Color,
+        defaultValue: "#f7e8f6",
+    },
+
+    boundDisplay: {
+        title: "Display Bounds?",
+        type: ControlType.Boolean,
+        defaultValue: true,
+    },
+
+    radius: {
+        title: "Radius",
+        type: ControlType.Number,
+        min: 0,
+        max: 50,
+        step: 2,
+        defaultValue: 50,
+        displayStepper: true,
+    },
+})
